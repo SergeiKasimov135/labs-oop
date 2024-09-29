f1e14d62c8d46bb6ee6ac0a7d0fea211f20cef74
Sergei Kasimov <101012933+SergeiKasimov135@users.noreply.github.com>
2024-09-29 19:27:28 +0400

    Kasimov: delete odd directories

--- labs-oop/src/main/java/ru/ssau/tk/kasimov/labsoop/lab2/functions/implementations/UnitFunction.java
+++
@@ -1,7 +0,0 @@
-package ru.ssau.tk.kasimov.labsoop.lab2.functions.implementations;
-
-public class UnitFunction extends ConstantFunction {
-    public UnitFunction() {
-        super(1);
-    }
-}
--- labs-oop/src/main/java/ru/ssau/tk/kasimov/labsoop/lab2/functions/implementations/ZeroFunction.java
+++
@@ -1,7 +0,0 @@
-package ru.ssau.tk.kasimov.labsoop.lab2.functions.implementations;
-
-public class ZeroFunction extends ConstantFunction {
-    public ZeroFunction() {
-        super(0);
-    }
-}
