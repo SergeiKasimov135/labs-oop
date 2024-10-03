b184b1f02f2cad3d71450635820eeeb8a1ad9bab
Sergei Kasimov <101012933+SergeiKasimov135@users.noreply.github.com>
2024-10-03 16:51:04 +0400

    fix ChebyshevInterpolation.java

--- labs-oop/src/main/java/ru/ssau/tk/kasimovserzhantov/labsoop/lab/functions/implementations/ChebyshevInterpolation.java
+++ labs-oop/src/main/java/ru/ssau/tk/kasimovserzhantov/labsoop/lab/functions/implementations/ChebyshevInterpolation.java
@@ -5,32 +5,25 @@ import ru.ssau.tk.kasimovserzhantov.labsoop.lab.functions.coredefenitions.interf
 public class ChebyshevInterpolation implements MathFunction {
     private final MathFunction function;
     private final double[] nodes;
-    private int n;
-    private double a;
-    private double b;
 
     public ChebyshevInterpolation(MathFunction function, double a, double b, int n) {
         this.function = function;
-        this.n = n;
-        this.a = a;
-        this.b = b;
         nodes = new double[n + 1];
         for (int i = 0; i <= n; i++) {
-            nodes[i] =  (b - a) / 2 * Math.cos(Math.PI * (2 * i + 1) / (2 * n + 2)) + (a + b) / 2;
+            nodes[i] = 0.5 * (a + b) + 0.5 * (b - a) * Math.cos((Math.PI * (2 * i + 1)) / (2 * n + 2));
         }
     }
 
-    @Override
     public double apply(double x) {
         double result = 0;
         for (int j = 0; j < nodes.length; j++) {
-            double lagrangeBasisPolynomial = function.apply(nodes[j]);
+            double product = function.apply(nodes[j]);
             for (int k = 0; k < nodes.length; k++) {
                 if (k != j) {
-                    lagrangeBasisPolynomial *= (x - nodes[k]) / (nodes[j] - nodes[k]);
+                    product *= (x - nodes[k]) / (nodes[j] - nodes[k]);
                 }
             }
-            result += lagrangeBasisPolynomial;
+            result += product;
         }
         return result;
     }
