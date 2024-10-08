61d7c1f21d042c713a085c4ce73fb7f7a55350d2
Serzhantov Andrey <postpressy@gmail.com>
2024-10-08 23:43:23 +0400

    Serzhantov (Y-part) Task 1 create class point and add exceptions

---
+++ labs-oop/src/main/java/ru/ssau/tk/kasimovserzhantov/labsoop/lab/functions/coredefenitions/Point.java
@@ -0,0 +1,20 @@
+package ru.ssau.tk.kasimovserzhantov.labsoop.lab.functions.coredefenitions;
+
+public class Point {
+
+    public final double x;
+    public final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
