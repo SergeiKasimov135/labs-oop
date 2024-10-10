571e10d3fecbe83128d1b1d6ee8fae445d34e0f5
Sergei Kasimov <101012933+SergeiKasimov135@users.noreply.github.com>
2024-10-10 17:33:15 +0400

    Update AbstractTabulatedFunction.java

--- labs-oop/src/main/java/ru/ssau/tk/kasimovserzhantov/labsoop/lab/functions/coredefenitions/abstractclasses/AbstractTabulatedFunction.java
+++ labs-oop/src/main/java/ru/ssau/tk/kasimovserzhantov/labsoop/lab/functions/coredefenitions/abstractclasses/AbstractTabulatedFunction.java
@@ -1,10 +1,26 @@
 package ru.ssau.tk.kasimovserzhantov.labsoop.lab.functions.coredefenitions.abstractclasses;
 
+import ru.ssau.tk.kasimovserzhantov.labsoop.lab.exceptions.ArrayIsNotSortedException;
+import ru.ssau.tk.kasimovserzhantov.labsoop.lab.exceptions.DifferentLengthOfArraysException;
+import ru.ssau.tk.kasimovserzhantov.labsoop.lab.functions.coredefenitions.Point;
 import ru.ssau.tk.kasimovserzhantov.labsoop.lab.functions.coredefenitions.interfaces.TabulatedFunction;
 
 public abstract class AbstractTabulatedFunction implements TabulatedFunction {
 
     protected int count;
+
+    public static void checkLengthIsTheSame(double[] xValues, double[] yValues) {
+        if (xValues.length != yValues.length) {
+            throw new DifferentLengthOfArraysException("Arrays must be the same length");
+        }
+    }
+
+    public static void checkSorted(double[] xValues) {
+        for (int i = 1; i < xValues.length; ++i) {
+            if (xValues[i] < xValues[i - 1])
+                throw new ArrayIsNotSortedException("Array must be sorted in increasing order");
+        }
+    }
 
     protected abstract int floorIndexOfX(double x);
 
@@ -34,4 +50,13 @@ public abstract class AbstractTabulatedFunction implements TabulatedFunction {
         }
     }
 
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder(getClass().getSimpleName() + " size = " + getCount() + "\n");
+        for (Point point : this) {
+            string.append("[").append(point.getX()).append("; ").append(point.getY()).append("]\n");
+        }
+
+        return string.toString();
+    }
 }
