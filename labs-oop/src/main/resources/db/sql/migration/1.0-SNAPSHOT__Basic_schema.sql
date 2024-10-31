2267459e324efb7dd162ebb5eb778fae6b2d3642
Sergei Kasimov <101012933+SergeiKasimov135@users.noreply.github.com>
2024-10-31 09:20:04 +0400

    fix 1.0-SNAPSHOT__Basic_schema.sql

--- labs-oop/src/main/resources/db/sql/migration/1.0-SNAPSHOT__Basic_schema.sql
+++ labs-oop/src/main/resources/db/sql/migration/1.0-SNAPSHOT__Basic_schema.sql
@@ -4,14 +4,14 @@ create table labs.t_function(
     id serial primary key,
     c_function_type varchar(255),
     c_count integer check ( c_count >= 2 ),
-    c_x_from double,
-    c_x_to double
+    c_x_from double precision,
+    c_x_to double precision
 );
 
 create table labs.t_point(
     id serial primary key,
-    function_id int,
-    c_x_value double,
-    c_y_value double,
+    function_id integer,
+    c_x_value double precision,
+    c_y_value double precision,
     foreign key (function_id) references labs.t_math_function(id) on delete cascade
 );
